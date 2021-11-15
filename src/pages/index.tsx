ca35ac3f793c800343989b437b5f2292f3c43232
fengkiej <fengkiejunis@gmail.com>
2021-11-15 16:44:21 +0700

    :lipstick: add layouting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Button from '../components/Button'
+import Hero from '../components/Hero'
+import Menu from '../components/Menu'
+import Review from '../components/Review'
+import ContactUs from '../components/ContactUs'
+import Footer from '../components/Footer'
+import Nav from '../components/Nav'
 
 const Home: NextPage = () => {
   return (
@@ -8,10 +13,14 @@ const Home: NextPage = () => {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" /> 
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Playfair+Display&family=Roboto:wght@300&display=swap" rel="stylesheet" /> 
       </Head>
-      <Button>Find Us</Button>
-      <Button>Our Menu</Button>
+      <Nav id="nav" />
+      <Hero id="hero" />
+      <Menu id="menu" />
+      <Review id="review" />
+      <ContactUs id="contact" />
+      <Footer />
     </div>
   )
 }
